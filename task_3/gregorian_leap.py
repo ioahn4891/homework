961143feae2be6eccec5665122848c129091d3ac
ioahn4891 <ioahn4891@gmail.com>
2023-11-30 16:01:35 +0200

    перевірка перед здачею

--- task_3/gregorian_leap.py
+++ task_3/gregorian_leap.py
@@ -4,13 +4,12 @@ if 1900 < year < 1_000_000:
 
     if year % 4 != 0:
         print(year, "is not leep year")
+
     elif year % 100 == 0:
         if year % 400 == 0:
             print(year, "is Leap Year!")
-        else:
-            print(year, "is not leep year")
     else:
-        print(year, "is Leap Year!")
+        print(year, "is leep year")
 
 else:
     print("не вірний діапазон!!!")
